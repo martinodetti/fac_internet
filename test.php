886eb94d38a6e2f30b74eb26c81cd65ad0785b7b
root <root@frenosoeste-srv>
2019-11-07 12:17:28 -0300

    otro cliente con factura de credito electronica

--- test.php
+++ test.php
@@ -1,10 +1,12 @@
 <?php
 include_once 'DAC/Database.class.php';
+include_once 'CONTROLLER/C_Debug.php';
+
 include 'afip/awsfe.php';
 
 $acon = new awsfe();
 
-$param['FeCompConsReq'] = array( 'CbteNro'   => 12,
+$param['FeCompConsReq'] = array( 'CbteNro'   => 169,
                                  'PtoVta'    => 2,
                                  'CbteTipo'  => 6);
 
