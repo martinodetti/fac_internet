e23471c617509a84a517d395a546d0c31fbb9297
Martin Odetti <martin.odetti@gmail.com>
2024-12-09 13:08:54 -0300

    correcciones en el mail

--- mail_sender.php
+++ mail_sender.php
@@ -53,14 +53,14 @@ class mail_sender{
             $this->mail->addAddress($cliente->get_email_persona());
 
             //Set the subject line
-            $this->mail->Subject = 'Nuevo documento de Frenos Oeste, ref: ' . $numero . '. ';
+            $this->mail->Subject = 'Nuevo comprobante de Frenos Oeste, ref: ' . $numero . '. ';
 
             //Read an HTML message body from an external file, convert referenced images to embedded,
             //convert HTML into a basic plain-text alternative body
             //$mail->msgHTML(file_get_contents('contents.html'), __DIR__);
             
             $this->mail->msgHTML('<p>Estimado cliente,</p>
-        <p>Adjunto encontrará el documento solicitado.</p>
+        <p>Adjunto encontrará el comprobante emitido.</p>
         <p>Cualquier inquietud enviar mail a las siguientes direcciones:</p>
         <p>clulic@frenosoeste.com.ar</p>
         <p>info@frenosoeste.com.ar</p>
